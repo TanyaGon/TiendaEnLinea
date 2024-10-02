3b68ed6394da07def7f61cedbce412d0381029d1
Sebascode20 <sebastiancrack20@outlook.com>
2024-10-01 22:15:28 -0600

    crear el repository y las clases y el metod show de ProductoController

--- src/main/java/sv/edu/udb/TiendaEnLineaApplication.java
+++ src/main/java/sv/edu/udb/TiendaEnLineaApplication.java
@@ -2,8 +2,9 @@ package sv.edu.udb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class TiendaEnLineaApplication {
 
 	public static void main(String[] args) {
