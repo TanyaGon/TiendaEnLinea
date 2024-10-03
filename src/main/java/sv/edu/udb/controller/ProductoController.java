7bd115f9757ad0e132ebde1add2ccf138b8b4927
Sebascode20 <sebastiancrack20@outlook.com>
2024-10-03 15:01:56 -0600

    funcionalidad para eliminar un producto

--- src/main/java/sv/edu/udb/controller/ProductoController.java
+++ src/main/java/sv/edu/udb/controller/ProductoController.java
@@ -5,11 +5,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import sv.edu.udb.model.Producto;
 import sv.edu.udb.model.Usuario;
 import sv.edu.udb.service.ProductoService;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/productos")
@@ -40,6 +43,32 @@ public class ProductoController {
         producto.setUsuario(u);
 
         productoService.save(producto);
+        return "redirect:/productos";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable Integer id, Model model){
+        Producto producto = new Producto();
+
+        Optional<Producto> optionalProducto = productoService.get(id);
+
+        producto = optionalProducto.get(); //Obtiene el producto que hemos mandado a buscar
+
+        LOGGER.info("Producto buscado: {}", producto);
+        model.addAttribute("producto",producto);
+
+        return "/administrador/productos/edit";
+    }
+
+    @PostMapping("/update")
+    public String update(Producto producto){
+        productoService.update(producto);
+        return "redirect:/productos";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable  Integer id){
+        productoService.delete(id);
         return "redirect:/productos";
     }
 }
